f57686258c138527f6a559973a4c6de2007a656c
1dam <1dam@HZ301198.TartangaLH.eus>
2021-05-24 09:31:49 +0200

    Commit de javadoc

--- ControlVersiones/src/controlVersiones/Empleado.java
+++ ControlVersiones/src/controlVersiones/Empleado.java
@@ -1,17 +1,31 @@
 package controlVersiones;
 
 public class Empleado {
+	
+	/**
+	 * @author UnaiUrtiaga
+	 * @version Version 1, 2021
+	 * @since 24/05/2021
+	 * 
+	 */
+
 	private String nombre;
 	private String apellidos;
 	private double salario; 
+	
+	/**
+	 * @param nombre es una atributo tipo string que se referirá al nombre del empleado
+	 * @param apellido es un atributo tipo string que se referirá a los apellidos del empleado
+	 * @param salario es un atributo tipo double (numeros) que se referirá al salario del empleado
+	 */
+	
 
-
+	
 public Empleado(String nombre, String apellido, double salario) {
 	this.nombre = nombre;
 	this.apellidos = apellido;
 	this.salario = salario;
 }
-
 
 
 public String getNombre() {
@@ -33,9 +47,13 @@ public void setSalario(double salario) {
 	this.salario = salario;
 }
 
+
 public void subirsalario (double subida) {
 	salario=salario+subida;
 }
 
+/**
+ * subirsalario es un metodo el cual aumenta la cantidad de la variable salario con un parametro enviado por otra clase
+ */
 
 }
